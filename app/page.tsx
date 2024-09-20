21c6051f5925b81c821867206033e3ab31ae3255
Luis Martinez <luismartinezwebdev@gmail.com>
2024-09-20 13:55:33 +0700

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,18 @@ export default function Home() {
   return (
     <div className="relative w-full h-[calc(100vh-10rem)] bg-gradient-to-br from-gray-900 to-black flex items-center justify-center">
       <div className="text-center">
-        <h1 className="text-5xl md:text-6xl font-bold text-white mb-8">
-          No procrastination today!
+        <h1 className="text-5xl md:text-6xl font-bold text-white mb-8 capitalize">
+          start focusing now!
         </h1>
+        <p className="text-lg text-gray-300 mb-8">Overcome morning lack of focus, avoid randomly picking a task, start focusing on the right thing now</p>
         <Link
           href="/daily"
-          className="inline-block bg-white text-gray-900 font-semibold py-3 px-8 rounded-lg hover:bg-gray-100 transition-colors duration-300"
+          className="inline-block text-white font-semibold py-4 px-10 rounded-lg text-lg shadow-lg relative overflow-hidden"
         >
-          Get started now
+          <span className="relative z-10">Start Your Productive Day</span>
+          <div className="absolute inset-0 bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 bg-animated"></div>
         </Link>
+        <p className="mt-4 text-sm text-gray-400">It's free!</p>
       </div>
     </div>
   );
